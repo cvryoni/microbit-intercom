97da426767f81145c3824b28563a63b249d5c828
cvryoni <121550180+cvryoni@users.noreply.github.com>
2022-12-28 13:35:27 +0200

    Added sound

--- main.ts
+++ main.ts
@@ -1,31 +1,34 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     if (icon > 0) {
         icon = icon - 1
     }
-    
     basic.showIcon(icons[icon])
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+input.onButtonPressed(Button.AB, function () {
+    radio.sendValue("icon", icon)
+})
+input.onButtonPressed(Button.B, function () {
     if (icon < icons.length - 1) {
         icon = icon + 1
     }
-    
     basic.showIcon(icons[icon])
 })
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
-    radio.sendValue("icon", icon)
-})
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
-    
+radio.onReceivedValue(function (name, value) {
+    music.setVolume(212)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 4035, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
     if (name == "icon") {
         basic.showIcon(icons[value])
-        icon = value
+icon = value
     }
-    
 })
+let icons: number[] = []
 let icon = 0
-let icons = [IconNames.Happy, IconNames.Heart, IconNames.Angry, IconNames.Butterfly, IconNames.Yes, IconNames.No]
+icons = [
+IconNames.Happy,
+IconNames.Heart,
+IconNames.Angry,
+IconNames.Butterfly,
+IconNames.Yes,
+IconNames.No
+]
 icon = 0
